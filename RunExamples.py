be33cafda575f4e8e7cd816014cc6b66da5af5bd
lupin3000 <steffenlorenz@hotmail.de>
2022-03-18 15:40:43 +0100

    enable aug cat

--- RunExamples.py
+++ RunExamples.py
@@ -16,6 +16,7 @@ from src.examples.other_detection.mediapipe_finger_count import FingerCount
 from src.examples.other_detection.mediapipe_hand_landmark import HandLandmark
 from src.examples.other_detection.opencv_color_detection import Color
 from src.examples.other_detection.cvzone_hand_tracking import CVZHandTracking
+from src.examples.aug.face_filter import FaceFilter
 from src.examples.road.lane_detection import LaneDetection
 from src.player.window import ShowPlayer
 
@@ -67,6 +68,8 @@ class Examples:
             player = CarDetection(title='[Haarcascade car detection]', cap=self.input_source)
         elif self.show_example == 'other-7':
             player = CVZHandTracking(title='[CVZone Hand tracking]', cap=self.input_source)
+        elif self.show_example == 'aug-1':
+            player = FaceFilter(title='[Haarcascade Filter]', cap=self.input_source)
         elif self.show_example == 'road-1':
             player = LaneDetection(title='[Road lane detection]', cap=self.input_source)
         else:
